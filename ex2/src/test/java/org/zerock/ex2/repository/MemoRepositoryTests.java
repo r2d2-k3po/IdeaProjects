bd6dfb020942b5fde1c5c47d43bf2c95b8aa51f6
r2d2-k3po <r2d2factory3@gmail.com>
2022-02-22 17:54:00 +0900

    ex2_2

--- ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -3,6 +3,10 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex2.entity.Memo;
 
@@ -61,5 +65,39 @@ public class MemoRepositoryTests {
         Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
 
         System.out.println(memoRepository.save(memo));
+    }
+
+    @Test
+    public void testDelete() {
+
+        Long mno = 100L;
+
+        memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault() {
+
+        Pageable pageable = PageRequest.of(9,10);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        System.out.println(result);
+
+        for (Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSort() {
+
+        Sort sort1 = Sort.by("mno").descending();
+        Pageable pageable = PageRequest.of(0,10, sort1);
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(memo -> {
+            System.out.println(memo);
+        });
     }
 }
