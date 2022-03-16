ecee9edb9507c07bfb214fd0a49962871ceba649
r2d2-k3po <r2d2factory3@gmail.com>
2022-03-16 17:45:16 +0900

    p493

---
+++ club/src/test/java/orz/zerock/club/security/PasswordTests.java
@@ -0,0 +1,30 @@
+package orz.zerock.club.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+public class PasswordTests {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void testEncode() {
+
+        String password = "1111";
+
+        String enPw = passwordEncoder.encode(password);
+
+        System.out.println("enPw: " + enPw);
+
+        boolean matchResult = passwordEncoder.matches(password, enPw);
+
+        System.out.println("matchResult: " + matchResult);
+
+    }
+
+}
+// $2a$10$CDgg6eV7t41zVebvf74LsuyIhdYFnb3bOqnO5lesUv4/jAYUUJ9Nu
